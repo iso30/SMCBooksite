6036aee1240c9814a7f6421ac99095a470afcb71
Mohammed Azad <Varsity.azad@gmail.com>
2016-02-15 20:31:04 -0800

    added users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			login(@user)
+			redirect_to user_path(@user.id)
+		else
+			flash[:danger] = "Invalid signup"
+			redirect_to signup_path
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def destroy
+
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username,:password_digest,:email,:show_email)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new' 
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  resources :users, only:[:create,:show,:destroy]
+  get 'signup' => 'users#new'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
